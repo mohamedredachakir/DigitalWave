07e6181f04d6107379c603732fe6e9cdde6f4644
mohamedredachakir <chakirm082@gmail.com>
2025-12-01 11:56:49 +0100

    create first condition for all pages to log in pages

---
+++ public/index.php
@@ -0,0 +1,36 @@
+
+
+
+<?php 
+
+$page = $_GET['page'] ?? 'home';
+
+switch ($page){
+      case 'home':
+        $view = '../views/home.php';
+        $title = 'Accueil';
+        break;
+
+    case 'about':
+        $view = '../views/about.php';
+        $title = 'À propos';
+        break;
+
+    case 'services':
+        $view = '../views/services.php';
+        $title = 'Services';
+        break;
+
+    case 'contact':
+        $view = '../views/contact.php';
+        $title = 'Contact';
+        break;
+
+    default:
+        $view = '../views/404.php';
+        $title = 'Page introuvable';
+}
+
+include '../DigitalWave/views/layout.php'
+
+?>
